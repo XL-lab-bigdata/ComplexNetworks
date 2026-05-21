da6fed1d2a8e691f5873e9c405b43ab9f1d4211d
piziZero <115726112+piziZero@users.noreply.github.com>
2026-05-21 19:40:15 +0800

    Delete chapter5 directory

--- chapter5/code/5-2  R.py
+++
@@ -1,3 +0,0 @@
-from sklearn.metrics import auc #导入auc函数
-R =  auc([x / len(G) for x in range(len(G) + 1)], largest_all)
-# 使用sklearn.metrics模块中的AUC函数来计算鲁棒性曲线下的面积。其中，largest_all是一个记录逐个删除节点时网络相对最大连通片规模的列表，x / len(G)表示删除节点的比例。瓦解过程代码详见5.3节文件5-9 malicious.py。
